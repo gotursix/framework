08bac5a4408ec3210e223f353cbb9c322fef6c3a
Zeul <vly.2814@gmail.com>
2019-01-25 18:51:00 +0200

    fix

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
-
 <?php
-echo "That looks pretty good tho";
+session_start();
 
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT',dirname(__FILE__));
+$url = isset($_SERVER['PATH_INFO']) ? explode('/',ltrim($_SERVER['PATH_INFO'],'/')) :[] ;
+
+require_once(ROOT . DS . 'core' . DS . 'bootstrap.php');
 
  ?>
