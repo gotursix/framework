053235ea92c0d9412dc531403ab0933daba1985a
Zeul <vly.2814@gmail.com>
2019-01-25 19:04:06 +0200

    schelet

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,1 +1,10 @@
-Hello nigger bootstrap
+<?php
+//load configuration and helper functions
+
+require_once(ROOT . DS . 'config' . DS . 'config.php');
+require_once(ROOT . DS . 'app' . DS . 'lib' . DS . 'helpers' . DS . 'functions.php');
+
+//Autoload classes
+
+
+ ?>
