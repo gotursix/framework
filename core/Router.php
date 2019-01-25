0b659bbd925fc46199cca02602e8706e4f1a6221
Zeul <vly.2814@gmail.com>
2019-01-25 20:05:07 +0200

    router+dnd

---
+++ core/Router.php
@@ -0,0 +1,9 @@
+<?php
+
+class Router
+{
+  public static function route($url)
+    {
+      dnd($url);
+    }
+}
--- index.php
+++ index.php
@@ -4,7 +4,6 @@ session_start();
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT',dirname(__FILE__));
 $url = isset($_SERVER['PATH_INFO']) ? explode('/',ltrim($_SERVER['PATH_INFO'],'/')) :[] ;
-
 require_once(ROOT . DS . 'core' . DS . 'bootstrap.php');
 
  ?>
