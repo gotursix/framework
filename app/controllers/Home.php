d396574d7763c6766eb3e4fa66c3542f8537a11d
Zeul <vly.2814@gmail.com>
2019-01-26 15:35:13 +0200

    index action

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,4 +3,16 @@
 class Home extends Controller
 {
 
+public function __construct($controller,$action)
+  {
+      parent::__construct($controller,$action);
+  }
+
+public function indexAction()
+  {
+    die('Welcome to the home controller , this is the index action.');
+  }
+
+
+
 }
