6b83e60efc176f63a1adbc06d530ebf2c3e61cff
Zeul <vly.2814@gmail.com>
2019-01-26 19:08:52 +0200

    added db and connection

--- config/config.php
+++ config/config.php
@@ -1,6 +1,13 @@
 <?php
 
+
 define('DEBUG', true );
+
+define('DB_NAME', 'hell'); //database name
+define('DB_USER', 'root'); //database user
+define('DB_PASSWORD', ''); //database password
+define('DB_HOST', '127.0.0.1'); //database host ***use IP address to avoid DNS lookup
+
 define('DEFAULT_CONTROLLER','Home'); //default controller if there isn't one in the url
 define('DEFAULT_LAYOUT','default'); //if no layout is set in the controller use this s_layout
 define('PROOT','/framework/'); //set this to '/' for a live server
