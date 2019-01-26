737571a1d5231ea38948e1bdd0e3c4ddd8b171a6
Zeul <vly.2814@gmail.com>
2019-01-26 21:26:36 +0200

    show from db

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -10,6 +10,12 @@ public function __construct($controller,$action)
 
 public function indexAction()
   {
+   $db = DB::getInstance();
+   $sql="SELECT * FROM CONTACTS";
+   $contactsQ = $db->query($sql);
+   dnd($contactsQ);
    $this->view->render('home/index');
   }
+
+
 }
