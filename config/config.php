c4875673ec0f1ec60d4b204d3607d1b1c90fc8bc
Zeul <vly.2814@gmail.com>
2019-01-26 17:40:41 +0200

    fix config

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 
- define('DEBUG', true );
+define('DEBUG', true );
 define('DEFAULT_CONTROLLER','Home'); //default controller if there isn't one in the url
 define('DEFAULT_LAYOUT','default'); //if no layout is set in the controller use this s_layout
 define('PROOT','/framework/'); //set this to '/' for a live server
