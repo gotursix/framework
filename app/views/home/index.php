6f21dad732b5a549b2563d947eebee3947b7e813
Zeul <vly.2814@gmail.com>
2019-01-26 16:31:56 +0200

    deleting useless files

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,5 +1,5 @@
 <?php //$this->setSiteTitle('Home'); //set every spage title ?>
 
 <?php $this->start('body'); ?>
-<h1>Welcome to Rufus Framework</h1>
+<h1 class="text-center red">Welcome to Rufus Framework</h1>
 <?php $this->end(); ?>
