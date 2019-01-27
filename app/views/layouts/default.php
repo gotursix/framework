af00bf4dc05c020a1980c8aa447c6ec3c90aebfb
Zeul <vly.2814@gmail.com>
2019-01-27 12:33:31 +0200

    bootstrap + jquery 3.3.1

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -5,18 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$this->siteTitle(); ?></title>
-    <link href="<?=PROOT?>css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?=PROOT?>css/custom.css" rel="stylesheet">
-    <script src="<?=PROOT?>js/jquery-2.2.4.min.js"></script>
-    <script src="<?=PROOT?>js/bootstrap.min.js"></script>
-
+    <link href="<?=PROOT?>css/bootstrap.css" rel="stylesheet">
+    <script src="<?=PROOT?>js/jquery-3.3.1.js"></script>
+    <script src="<?=PROOT?>js/bootstrap.js"></script>
 <?= $this->content('head'); ?>
-
   </head>
   <body>
     <?= $this->content('body'); ?>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
