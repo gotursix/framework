16eb46c5050c11be15cc4eb8e18d31cc2c48ac24
Zeul <vly.2814@gmail.com>
2019-01-27 12:38:15 +0200

    added subprogram

---
+++ usefull/index.php
@@ -0,0 +1,18 @@
+<?php $this->setSiteTitle('Home'); ?>
+
+<?php $this->start('head'); ?>
+<script>//here java script,next row css</script>
+<style></style>
+<?php $this->end(); ?>
+
+<?php $this->start('body'); ?>
+<h1>Welcome to Rufus Framework</h1>
+<?php $this->end(); ?>
+
+
+//Get param url
+public function indexAction($name)
+  {
+   echo $name;
+   $this->view->render('home/index');
+  }
