c6d9f19c9497fabc570843b9945b84ad6d28fa80
Zeul <vly.2814@gmail.com>
2019-01-30 15:12:42 +0200

    finished model

--- core/Model.php
+++ core/Model.php
@@ -2,7 +2,7 @@
 
 class Model{
 
-protected $_db, $_table, $modelName, $_softDelete, $_columnNames=[];
+protected $_db, $_table, $_modelName, $_softDelete = false , $_columnNames=[];
 public $id;
 
 
@@ -54,10 +54,89 @@ public $id;
       return $result;
     }
 
+
     public function findById($id)
     {
       return $this->findFirst(['conditions'=>"id = ?",'bind'=>[$id]]);
     }
+
+
+    public function save()
+    {
+      $fields=[];
+      foreach ($this->_columnNames as $column)
+      {
+         $fields[$column]= $this->$column;
+      }
+      //determine whether we update or insert
+      if(property_exists($this,'id') && $this->id != '')
+        {
+          return $this->update($this->id, $fields);
+        }
+        else
+        {
+          return $this->insert($fields);
+        }
+    }
+
+
+  public function insert($fields)
+     {
+       if(empty($fields))return falsel
+       return $this->_db->insert($this->_table, $fields);
+     }
+
+
+  public function update($id, $fields)
+     {
+       if(empty($fields) || $id=='')return false;
+       return $this->_db->update($this->_table , $id , fields);
+     }
+
+
+  public function delete($id='')
+     {
+        if($id == '' && $this->id=='') return false;
+        $id = (id= '')? $this->id : $id;
+      if($this->_softDelete)
+      {
+        $this->update($id, ['deleted'=> 1]);
+      }
+      return $this->_db->delete($this->_table, $id);
+     }
+
+    public function query($sql , $bind=[])
+    {
+      return $this->_db->query($sql, $bind);
+    }
+
+
+    public function data()
+    {
+      $data = new stdClass();
+      foreach ($this->_columnNames as $column)
+      {
+        $data->column = $this->column;
+      }
+      return $data;
+    }
+
+
+    public function assign ($params)
+     {
+       if(!empty($params))
+       {
+        foreach ($params as $key => $vaL)
+        {
+          if(in_array($key, $this->_columnNames))
+          {
+            $this->key = sanitize($val);
+          }
+        }
+        return true;
+        }
+        return false;
+     }
 
 
     protected function populateObjData($result)
@@ -68,7 +147,6 @@ public $id;
        }
 
      }
-
 
 
 }
