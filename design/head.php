78bb882ea6c909ee3078c3a6827d59a65f22db1b
Vally16768 <47054845+Vally16768@users.noreply.github.com>
2019-01-30 17:15:09 +0200

    imbinare
    design la locul lui

--- design/head.php
+++ design/head.php
@@ -2,9 +2,9 @@
 <title>Rufus Framework</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-  <link rel="stylesheet" type="text/css" href="font-awesome/css/font-awesome.min.css">
-  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+   <link href="<?=PROOT?>css/custom.css" rel="stylesheet">
+  <link href="<?=PROOT?>css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?=PROOT?>css/font-awesome.min.css" rel="stylesheet">
+  <script src="<?=PROOT?>bootstrap.min.js"></script>
+  <script src="<?=PROOT?>jQuery-3.3.1.js"></script>
 </head>
