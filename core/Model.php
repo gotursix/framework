c11d2ac3d7686e67db5f066be9196a040edf1fb1
Zeul <vly.2814@gmail.com>
2019-01-31 18:09:26 +0200

    styled code

--- core/Model.php
+++ core/Model.php
@@ -4,16 +4,16 @@ class Model {
   public $id;
 
 
-  public function __construct($table)
-        {
-          $this->_db = DB::getInstance();
-          $this->_table = $table;
-          $this->_setTableColumns();
-          $this->_modelName = str_replace(' ','',ucwords(str_replace('_',' ',$this->_table)));
-        }
+    public function __construct($table)
+          {
+            $this->_db = DB::getInstance();
+            $this->_table = $table;
+            $this->_setTableColumns();
+            $this->_modelName = str_replace(' ','',ucwords(str_replace('_',' ',$this->_table)));
+          }
 
 
-    protected function _setTableColumns()
+        protected function _setTableColumns()
              {
                $columns = $this->get_columns();
                foreach ($columns as $column)
@@ -25,13 +25,13 @@ class Model {
              }
 
 
-  public function get_columns()
-  {
-    return $this->_db->get_columns($this->_table);
-  }
+             public function get_columns()
+             {
+               return $this->_db->get_columns($this->_table);
+             }
 
 
-  public function find($params = [])
+    public function find($params = [])
     {
       $results = [];
       $resultsQuery = $this->_db->find($this->_table, $params);
@@ -45,98 +45,98 @@ class Model {
     }
 
 
-  public function findFirst($params = [])
-  {
-    $params = $this->_softDeleteParams($params);
-    $resultQuery = $this->_db->findFirst($this->_table, $params,get_class($this));
-    return $resultQuery;
-  }
-
-
-  public function findById($id)
+    public function findFirst($params = [])
     {
-      return $this->findFirst(['conditions'=>"id = ?",'bind'=>[$id]]);
+      $params = $this->_softDeleteParams($params);
+      $resultQuery = $this->_db->findFirst($this->_table, $params,get_class($this));
+      return $resultQuery;
     }
 
 
-    public function save()
-        {
-          $fields=[];
-          foreach ($this->_columnNames as $column)
+    public function findById($id)
+      {
+        return $this->findFirst(['conditions'=>"id = ?",'bind'=>[$id]]);
+      }
+
+
+       public function save()
           {
-             $fields[$column]= $this->$column;
+            $fields=[];
+            foreach ($this->_columnNames as $column)
+            {
+              $fields[$column]= $this->$column;
+            }
+            //determine whether we update or insert
+            if(property_exists($this,'id') && $this->id != '')
+              {
+                return $this->update($this->id, $fields);
+              }
+              else
+              {
+                return $this->insert($fields);
+              }
+            }
+
+
+        protected function _softDeleteParams($params)
+        {
+          if($this->_softDelete)
+          {
+            if(array_key_exists('conditions',$params))
+            {
+              if(is_array($params['conditions']))
+              {
+                $params['conditions'][] = "deleted != 1";
+              } else {
+                $params['conditions'] .= " AND deleted != 1";
+              }
+            } else {
+              $params['conditions'] = "deleted != 1";
+            }
           }
-          //determine whether we update or insert
-          if(property_exists($this,'id') && $this->id != '')
-            {
-              return $this->update($this->id, $fields);
-            }
-            else
-            {
-              return $this->insert($fields);
-            }
+          return $params;
         }
 
 
-  protected function _softDeleteParams($params)
-  {
-  if($this->_softDelete)
-  {
-    if(array_key_exists('conditions',$params))
-    {
-      if(is_array($params['conditions']))
+        public function insert($fields)
+          {
+            if(empty($fields))return false;
+            return $this->_db->insert($this->_table, $fields);
+          }
+
+
+        public function update($id, $fields)
+            {
+              if(empty($fields) || $id=='')return false;
+              return $this->_db->update($this->_table , $id , fields);
+            }
+
+
+            public function delete($id='')
+            {
+              if($id == '' && $this->id=='') return false;
+              $id = ($id= '')? $this->id : $id;
+              if($this->_softDelete)
+              {
+                $this->update($id, ['deleted'=> 1]);
+              }
+              return $this->_db->delete($this->_table, $id);
+            }
+            public function query($sql , $bind=[])
+            {
+              return $this->_db->query($sql, $bind);
+            }
+
+
+    public function data()
       {
-        $params['conditions'][] = "deleted != 1";
-      } else {
-        $params['conditions'] .= " AND deleted != 1";
+        $data = new stdClass();
+        foreach ($this->_columnNames as $column)
+        {
+          $data->column = $this->column;
+        }
+        return $data;
       }
-    } else {
-      $params['conditions'] = "deleted != 1";
-    }
-  }
-  return $params;
-}
-
-
-public function insert($fields)
-   {
-     if(empty($fields))return false;
-     return $this->_db->insert($this->_table, $fields);
-   }
-
-
-public function update($id, $fields)
-   {
-     if(empty($fields) || $id=='')return false;
-     return $this->_db->update($this->_table , $id , fields);
-   }
-
-
-public function delete($id='')
-   {
-      if($id == '' && $this->id=='') return false;
-      $id = ($id= '')? $this->id : $id;
-    if($this->_softDelete)
-    {
-      $this->update($id, ['deleted'=> 1]);
-    }
-    return $this->_db->delete($this->_table, $id);
-   }
-  public function query($sql , $bind=[])
-  {
-    return $this->_db->query($sql, $bind);
-  }
-
-
-  public function data()
-    {
-      $data = new stdClass();
-      foreach ($this->_columnNames as $column)
-      {
-        $data->column = $this->column;
-      }
-      return $data;
-    }
 
 
     public function assign ($params)
@@ -155,15 +155,13 @@ public function delete($id='')
         return false;
      }
 
-     
-    protected function populateObjData($result)
+
+     protected function populateObjData($result)
      {
        foreach ($result as $key => $val)
        {
          $this->$key = $val;
        }
      }
-
-
 
 }
