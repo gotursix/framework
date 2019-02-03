ad22b484310b7d646a05ad1dbb8f064e28e5c9c7
Zeul <vly.2814@gmail.com>
2019-02-03 20:37:03 +0200

    added

--- core/Router.php
+++ core/Router.php
@@ -86,4 +86,53 @@ class Router
           return $grantAccess;
         }
 
+  public static function getMenu($menu)
+  {
+  $menuAry  = [];
+  $menuFile = file_get_contents(ROOT . DS . 'app' . DS . $menu . '.json');
+  $acl = json_decode($menuFile, true);
+
+  foreach($acl as $key => $val)
+  {
+    if(is_array($val)) {
+      $sub = [];
+      foreach($val as $k => $v) {
+        if($k == 'separator' && !empty($sub)) {
+          $sub[$k] = '';
+          continue;
+        }else if($finalVal = self::get_link($v)) {
+          $sub[$k] = $finalVal;
+        }
+      }
+      if(!empty($sub))
+      {
+        $menuAry[$key] = $sub;
+      }
+    }
+    else
+    {
+      if($finalVal = self::get_link($val))
+      {
+        $menuAry[$key] = $finalVal;
+      }
+    }
+  }
+  return $menuAry;
+}
+
+private static function get_link($val) {
+  //check if external link
+  if(preg_match('/https?:\/\//', $val) == 1) {
+    return $val;
+  } else {
+    $uAry = explode(DS, $val);
+    $controller_name = ucwords($uAry[0]);
+    $action_name = (isset($uAry[1]))? $uAry[1] : '';
+    if(Self::hasAccess($controller_name, $action_name)) {
+      return PROOT . $val;
+    }
+    return false;
+  }
+}
+
 }
