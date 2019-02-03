6344c4fb8b3a10b53a4fdceee36e44540d05b1ad
Zeul <vly.2814@gmail.com>
2019-02-03 20:38:07 +0200

    .

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$this->siteTitle(); ?></title>
     <link href="<?=PROOT?>css/custom.css" rel="stylesheet">
-    <link rel="stylesheet" href="h*ttps://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
