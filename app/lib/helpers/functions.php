710a8d797f54a04b5343ee96fb697ea33cbc5dd4
Zeul <vly.2814@gmail.com>
2019-02-05 15:08:34 +0200

    functions table

--- app/lib/helpers/functions.php
+++ app/lib/helpers/functions.php
@@ -1,3 +1,4 @@
 <?php
 require_once(ROOT . DS . 'app' . DS . 'lib' . DS . 'helpers' . DS . 'helpers.php');
- ?>
+require_once(ROOT . DS . 'app' . DS . 'lib' . DS . 'helpers' . DS . 'form_helpers.php');
+?>
