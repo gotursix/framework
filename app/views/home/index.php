4b3468c64c7831e57a90fca198ddf3faa7654d01
Zeul <vly.2814@gmail.com>
2019-02-05 18:06:10 +0200

    removed example

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -2,7 +2,4 @@
 
 <?php $this->start('body'); ?>
 <h1 class="text-center red">Welcome to Rufus Framework</h1>
-
-<?= inputBlock('text','Favorite Color:','favorite_color','red',['class'=>'form-control'],['class'=>'form-group']) ; ?>
-<?= submitBlock("Save",['class'=>'btn btn-primary'],['class'=>'text-right']); ?>
 <?php $this->end(); ?>
