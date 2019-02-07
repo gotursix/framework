e98d8dbd3236284f6d78abc9032eda217dbd74e8
Zeul <vly.2814@gmail.com>
2019-02-07 23:00:54 +0200

    fix register

--- app/views/Register/register.php
+++ app/views/Register/register.php
@@ -13,36 +13,36 @@
                             <div class="row">
                                 <div class="col-md-5 col-md-offset-1">
                                     <div class="card-content">
+                                      <div class="info info-horizontal">
+                                          <div class="icon icon-info"   style="margin-top: -0%;">
+                                              <i class="material-icons">E</i>
+                                          </div>
+                                          <div class="description">
+                                              <h4 class="info-title">asy to work</h4>
+                                              <p class="description">
+                                                  This Framework is easy to work on, because it follows the mvc structure.
+                                              </p>
+                                          </div>
+                                      </div>
                                         <div class="info info-horizontal">
                                             <div class="icon icon-rose">
-                                                <i class="material-icons">time</i>
+                                                <i class="material-icons" style="margin-top: -1%;">C</i>
                                             </div>
                                             <div class="description">
-                                                <h4 class="info-title">Marketing</h4>
+                                                <h4 class="info-title">reate albums</h4>
                                                 <p class="description">
-                                                    We've created the marketing campaign of the website. It was a very interesting collaboration.
+                                                    Create albums from your photos and videos.
                                                 </p>
                                             </div>
                                         </div>
                                         <div class="info info-horizontal">
                                             <div class="icon icon-primary">
-                                                <i class="material-icons">code</i>
+                                                <i class="material-icons" style="margin-top: -1%;">O</i>
                                             </div>
                                             <div class="description">
-                                                <h4 class="info-title">Fully Coded in HTML5</h4>
+                                                <h4 class="info-title">pen source</h4>
                                                 <p class="description">
-                                                    We've developed the website with HTML5 and CSS3. The client has access to the code using GitHub.
-                                                </p>
-                                            </div>
-                                        </div>
-                                        <div class="info info-horizontal">
-                                            <div class="icon icon-info">
-                                                <i class="material-icons">grup</i>
-                                            </div>
-                                            <div class="description">
-                                                <h4 class="info-title">Built Audience</h4>
-                                                <p class="description">
-                                                    There is also a Fully Customizable CMS Admin Dashboard for this product.
+                                                   The code can be found on Github, having presented how to set it up.
                                                 </p>
                                             </div>
                                         </div>
@@ -92,8 +92,13 @@
                                                 </span>
                                                 <input type="password" id="confirm" name="confirm" class="form-control" value="<?=$this->post['confirm']?>" placeholder="Confirm Password">
                                             </div>
+                                                <br><br>    <br><br>    <br><br>    <br><br>        <br><br>
+                                                  <div ><?= $this->displayErrors ?></div>
                                         </div>
-                                          <div ><?= $this->displayErrors ?></div>
+
+
+
+
                                         <div class="footer text-center">
                                               <input type="submit" class="btn btn-primary btn-round" value="Register">
                                         </div>
