b3d4a22b7eceb7e483aa919a08642884f3a79db6
Zeul <vly.2814@gmail.com>
2019-02-16 22:35:30 +0200

    Fix ref

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -7,7 +7,8 @@
     <title><?=$this->siteTitle(); ?></title>
     <link href="<?=PROOT?>css/custom.css" rel="stylesheet">
     <link href="<?=PROOT?>css/custom2.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?=PROOT?>css/bootstrap.min.css">
+    <link href="<?=PROOT?>css/bootstrap.min.css" rel="stylesheet">
+    <script src="<?=PROOT?>js/jQuery-3.3.1.js"?></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
