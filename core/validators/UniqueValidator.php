446302aef06f4d445e2943df165c690aa7f01387
Zeul <vly.2814@gmail.com>
2019-02-17 12:20:25 +0200

    Refactor Validation 3 fix

--- core/validators/UniqueValidator.php
+++ core/validators/UniqueValidator.php
@@ -2,36 +2,29 @@
 
 class UniqueValidator extends CustomValidator{
 
-  public function runValidation()
-  {
+  public function runValidation(){
     $field = (is_array($this->field))? $this->field[0] : $this->field;
     $value = $this->_model->{$field};
 
     $conditions = ["{$field} = ?"];
     $bind = [$value];
 
-    //check if we are updating
-    if(!empty($this->_model->id))
-    {
-      $conditions[] = "id = ?";
+    //check updating record
+    if(!empty($this->_model->id)){
+      $conditions[] = "id != ?";
       $bind[] = $this->_model->id;
     }
 
-    //this allows you to check multiple fields for unique
-
-    if(is_array($this->field))
-    {
+    //this allows you to check multiple fields for Unique
+    if(is_array($this->field)){
       array_unshift($this->field);
-      foreach ($this->field as $adds)
-      {
-        $conditions[] = "{$adds} = ?";
-        $bind = $this->_model->{$adds};
+      foreach($this->field as $adds){
+        $condtions[] = "{$adds} = ?";
+        $bind[] = $this->_model->{$adds};
       }
     }
-    H::dnd($bind);
-    $queryParams = ['condtions' => $conditions , 'bind' => $bind ];
+    $queryParams = ['conditions'=>$conditions,'bind'=>$bind];
     $other = $this->_model->findFirst($queryParams);
-
     return(!$other);
   }
 }
