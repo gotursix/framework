29ce46c6e5af8fe365b0ca9c23be5b661f101898
Andrew <dorinandries766@yahoo.com>
2019-02-22 19:27:52 +0200

    fix

--- index.php
+++ index.php
@@ -34,5 +34,6 @@ $url = isset($_SERVER['PATH_INFO']) ? explode('/',ltrim($_SERVER['PATH_INFO'],'/
 if(!Session::exists(CURRENT_USER_SESSION_NAME) && Cookie::exists(REMEMBER_ME_COOKIE_NAME)) {
   Users::loginUserFromCookie();
 }
+
 //Route the request
 Router::route($url);
