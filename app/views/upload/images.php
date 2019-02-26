3cee6bc99de889b9acff19e6d894639f7bdc548a
Popescu Valentin Dimitrie <popescuvalentin16768@gmail.com>
2019-02-26 11:05:37 +0200

    fix

--- app/views/upload/images.php
+++ app/views/upload/images.php
@@ -4,7 +4,6 @@ use App\Models\Upload;
  ?>
 <?php $this->start('body'); ?>
 <h2 class="text-center">My images</h2>
-<br>
 <table class="table table-striped" id="table">
 
  <thead class="thead-dark">
@@ -28,7 +27,31 @@ use App\Models\Upload;
      <td><?= $upload->name;?></td>
      <td><?= $upload->format;?></td>
      <?php $dir = Users::currentUser()->id; ?>
-     <td><img src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>"  height="70" width="70"></td>
+     <td>
+         <div class="row">
+  <div class="column">
+    <img src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>"  height="70" width="70" onclick="openModal();currentSlide(1)" class="hover-shadow cursor">
+  </div>
+         </div>
+         <div id="myModal" class="modal">
+  <span class="close cursor" onclick="closeModal()">&times;</span>
+     <div class="modal-content">
+
+    <div class="mySlides">
+      <img src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" >
+    </div>     
+          <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+    <div class="caption-container">
+      <p id="caption"></p>
+    </div>
+         <div class="column">
+      <img class="demo cursor" src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" style="width:100%" onclick="currentSlide(1)" alt="Nature and sunrise">
+    </div>
+             </div>
+         </div>
+             </td>
      <td> <a href="<?=PROOT?>upload/delete/<?=$upload->id?>" class="btn btn-danger btn-xs" onclick="if(!confirm('Are you sure ?')){return false;}">
          Delete
       </a></td>
