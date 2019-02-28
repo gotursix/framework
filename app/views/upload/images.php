d66b41db34ac8a00d3f5a7562b5218a53173a1c4
Zeul <vly.2814@gmail.com>
2019-02-27 22:36:18 -0800

    images

--- app/views/upload/images.php
+++ app/views/upload/images.php
@@ -6,60 +6,64 @@ use App\Models\Upload;
 <h2 class="text-center">My images</h2>
 <table class="table table-striped" id="table">
 
- <thead class="thead-dark">
 
-   <th>ID</th>
-   <th>USER_ID</th>
-   <th>NAME</th>
-   <th>FORMAT</th>
-   <th>IMG</th>
-   <th>BUTTons</th>
-   </thead>
+    <div class="container">
    <input type="text" id="search" placeholder="Type to search">
-   <tbody>
 
+   <br><br>
 
+        <div class="row">
 
     <?php foreach ($this->upload as $upload): ?>
-     <tr>
-     <td><?= $upload->id;   ?></td>
-     <td><?= $upload->user_id;?></td>
-     <td><?= $upload->name;?></td>
-     <td><?= $upload->format;?></td>
      <?php $dir = Users::currentUser()->id; ?>
-     <td>
-         <div class="row">
-  <div class="column">
-    <img src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>"  height="70" width="70" onclick="openModal();currentSlide(1)" class="hover-shadow cursor">
-      
+     <div class="col-sm-3">
+          <div class="thumbnail">
+            <img src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>"  height="190px"  onclick="openModal();currentSlide(1)" class="hover-shadow cursor">
+
+             <div class="caption">
+               <hr>
+          <p> <?= $upload->name;?> </p>
+
+
+                <div class="text-center">
+
+                  <a href="<?=PROOT?>upload/delete/<?=$upload->id?>" class="btn btn-danger btn-xs " onclick="if(!confirm('Are you sure ?')){return false;}">
+                    Delete
+                  </a>            </div>
+            </div>
+              </div>
+
+                        </div>
+
+                      <?php endforeach; ?>
+
+
+
+
+                         </div>
+
+
+              </div>
+                  </div>
+
+
   </div>
-         </div>
-         <div id="myModal" class="modal">
-     <div class="modal-content span3">
-  <span class="close cursor" onclick="closeModal()">&times;</span>
-    <div class="mySlides d-block">
-      <img src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>"  style="width:30%">
-    </div>     
-          <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-    <a class="next" onclick="plusSlides(1)">&#10095;</a>
 
-    <div class="caption-container">
-      <p id="caption"></p>
-    </div>
-         <div class="column">
-      <img class="demo cursor span3 d-block" src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" style="width:30%" onclick="currentSlide(1)">
-    </div>
-             </div>
-         </div>
-             </td>
-     <td> <a href="<?=PROOT?>upload/delete/<?=$upload->id?>" class="btn btn-danger btn-xs " onclick="if(!confirm('Are you sure ?')){return false;}">
-         Delete
-      </a></td>
 
-     <?php endforeach; ?>
-   </tr>
-   </tbody>
-</table>
+            </div>
+
+
+              </div>
+                  </div>
+
+
+
+
+ </div>
+</div>
+
+
+
 
 
 
