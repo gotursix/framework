a80b18064fb6e06c436aa3eccc29a9680787842d
Andrew <dorinandries766@yahoo.com>
2019-03-01 20:25:57 +0200

    fix

--- app/views/restricted/badToken.php
+++ app/views/restricted/badToken.php
@@ -1,4 +1,4 @@
 <?php $this->setSiteTitle('Bad Token'); ?>
 <?php $this->start('body'); ?>
-<h1 class="text-center red">Your token is corrupted.</h1>
+<h1 class="text-center red m-top">Your token is corrupted.</h1>
 <?php $this->end(); ?>
