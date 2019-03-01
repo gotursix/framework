dffa8ad44f07639bf3a4c1033282adcb53958243
Andrew <dorinandries766@yahoo.com>
2019-03-01 20:26:10 +0200

    fix on last fix

--- app/views/restricted/index.php
+++ app/views/restricted/index.php
@@ -1,4 +1,4 @@
 <?php $this->setSiteTitle('Access Restricted'); ?>
 <?php $this->start('body'); ?>
-<h1 class="text-center red">You do not have permission to access this page.</h1>
+<h1 class="text-center red m-top">You do not have permission to access this page.</h1>
 <?php $this->end(); ?>
