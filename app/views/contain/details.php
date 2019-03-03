9bb1b9b818d2c5104ed9316a43114c674f6c8d1e
Zeul <vly.2814@gmail.com>
2019-03-03 16:28:39 +0200

    Fix view album

--- app/views/contain/details.php
+++ app/views/contain/details.php
@@ -9,6 +9,7 @@ use App\Models\Users;
 
 <a href="<?=PROOT?>album" class="btn btn-default">Go back</a>
 
+<?php if ($this->album->format == 1): ?>
 <div class="row">
      <?php $x=1; ?>
         <?php foreach ($this->contain as $upload): ?>
@@ -19,12 +20,81 @@ use App\Models\Users;
                        <img src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" alt="Thumb-<?=$x?>" class="imgu">
                         </div>
                         <div class="caption text-center">
+                              <p><?=$upload->name?></p>
                    <hr>
                 </div>
              </div>
           <?php $x++; ?>
        <?php endforeach; ?>
    </div>
+ <?php endif; ?>
+
+
+
+ <?php if ($this->album->format == 2): ?>
+ <div class="row">
+      <?php $x=1; ?>
+         <?php foreach ($this->contain as $upload): ?>
+            <?php $dir = Users::currentUser()->id; ?>
+              <div class="col-sm-3">
+                  <hr>
+                    <div class="thumbnail text-center">
+                      <img src="<?= PROOT . 'img' . DS . 'video.png' ;?>" alt="Thumb-<?=$x?>" class="imgu">
+                         </div>
+                         <div class="caption text-center">
+                              <p><?=$upload->name?></p>
+                    <hr>
+                 </div>
+              </div>
+           <?php $x++; ?>
+        <?php endforeach; ?>
+    </div>
+  <?php endif; ?>
+
+
+
+  <?php if ($this->album->format == 3): ?>
+  <div class="row">
+       <?php $x=1; ?>
+          <?php foreach ($this->contain as $upload): ?>
+             <?php $dir = Users::currentUser()->id; ?>
+               <div class="col-sm-3">
+                   <hr>
+                     <div class="thumbnail text-center">
+                       <img src="<?= PROOT . 'img' . DS . 'video.png' ;?>" alt="Thumb-<?=$x?>" class="imgu">
+                          </div>
+                          <div class="caption text-center">
+                            <p><?=$upload->name?></p>
+
+                     <hr>
+                  </div>
+               </div>
+            <?php $x++; ?>
+         <?php endforeach; ?>
+     </div>
+   <?php endif; ?>
+
+
+
+   <?php if ($this->album->format == 4): ?>
+   <div class="row">
+        <?php $x=1; ?>
+           <?php foreach ($this->contain as $upload): ?>
+              <?php $dir = Users::currentUser()->id; ?>
+                <div class="col-sm-3">
+                    <hr>
+                      <div class="thumbnail text-center">
+                        <img src="<?= PROOT . 'img' . DS . 'document.png' ;?>" alt="Thumb-<?=$x?>" class="imgu">
+                           </div>
+                           <div class="caption text-center"
+                           <p><?=$upload->name?></p>
+                      <hr>
+                   </div>
+                </div>
+             <?php $x++; ?>
+          <?php endforeach; ?>
+      </div>
+    <?php endif; ?>
 
 </div>
 
