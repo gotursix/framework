083e3cbcaf86e6ce2a91f0f7f7e877c101bb1d0a
Zeul <vly.2814@gmail.com>
2019-03-04 01:12:01 -0800

    Design edit

--- app/views/album/edit.php
+++ app/views/album/edit.php
@@ -1,8 +1,4 @@
 <?php $this->setSiteTitle('Edit Contact'); ?>
 <?php $this->start('body'); ?>
-<br><br><br><br>
-
-<h2 class="text-center">Edit <?=$this->album->name ?></h2>
 <?php $this->partial('album','edit') ?>
-
 <?php $this->end(); ?>
