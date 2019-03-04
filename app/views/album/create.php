7aaac09ce26b5354366c83caed35ae825452e0d4
Zeul <vly.2814@gmail.com>
2019-03-04 00:58:27 -0800

    Design create album

--- app/views/album/create.php
+++ app/views/album/create.php
@@ -1,10 +1,4 @@
 <?php  $this->setSiteTitle('Add A Contact'); ?>
 <?php $this->start('body'); ?>
-
-<div >
-  <br>
-<h2 class="text-center">Add A contact</h2>
-<hr>
 <?php $this->partial('album','create'); ?>
-</div>
 <?php $this->end(); ?>
