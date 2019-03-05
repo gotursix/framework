e1e05861c1e995f0368ad0d0a6f09f7d1a29d96b
Zeul <vly.2814@gmail.com>
2019-03-05 22:09:13 +0200

    Refactor

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,63 +4,13 @@ use Core\Session;
 <?php //$this->setSiteTitle('Home'); //set every spage title ?>
 
 <?php $this->start('body'); ?>
+<br>
+<br>
+<br>
+<br>
 <h1 class="text-center red">Welcome to Rufus Framework</h1>
-<div onclick="ajaxTest();">Button me :>></div>
+<div class="center" onclick="ajaxTest();">Button me :>></div>
 
-<div class="container center">
-<div class="row right">
-<div class="col-sm-* col-centered">
-<div class="row">
-
-<div class="col card">
-<div class="thumbnail">
-<div class="caption">
-    <div class="card-body">
-<h3>Images</h3>
-        <a href="<?=PROOT?>upload/images"><img src="<?=PROOT?>img/picture.png"></a>
-</div>
-</div>
-</div>
-</div>
-
-<div class="col card">
-<div class="thumbnail">
-             <div class="caption">
-                 <div class="card-body">
-             <h3>Videos</h3>
-            <a href="<?=PROOT?>upload/video"><img src="<?=PROOT?>img/movie.png"></a>
-
-</div>
-</div>
-</div>
-</div>
-</div>
-<div class="row">
- <div class="col card">
-<div class="thumbnail">
-             <div class="caption">
-                 <div class="card-body">
-             <h3>Music</h3>
-    <a href="<?=PROOT?>upload/audio"><img src="<?=PROOT?>img/musical.png"></a>
-
-</div>
-</div>
-</div>
-</div>
-<div class="col card">
-<div class="thumbnail">
-             <div class="caption">
-                 <div class="card-body">
-             <h3>Documents</h3>
-    <a href="<?=PROOT?>upload/documents"><img src="<?=PROOT?>img/documents.png"></a>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
 
 <script>
     function ajaxTest(){
