53ca059f3ed375cbf61ebf11210e00b588762d03
Zeul <vly.2814@gmail.com>
2019-03-05 22:57:15 +0200

    Unused constant

--- config/config.php
+++ config/config.php
@@ -10,7 +10,6 @@ define('DB_HOST', '127.0.0.1'); //database host / use IP address to avoid DNS lo
 define('DEFAULT_CONTROLLER','Home'); //default controller if there isn't one in the url
 define('DEFAULT_LAYOUT','default'); //if no layout is set in the controller use this s_layout
 define('PROOT','/framework/'); //set this to '/' for a live server
-define('POOT','/framework/'); //set this to '/' for a live server
 define('SITE_TITLE','Rufus Framework'); //This will be used if no site title is set
 define('MENU_BRAND' ,'RUFUS'); // This is the brand text in menu
 
