8924d288f6bb312418fe49142a025f4dbbd16704
Popescu Valentin-Dimitrie <popescuvalentin16768@gmail.com>
2019-03-05 18:27:30 +0200

    greseala
    config host != config local host

--- config/config.php
+++ config/config.php
@@ -2,14 +2,15 @@
 
 define('DEBUG', true ); // show or not errors
 
-define('DB_NAME', 'u903322138_hell'); //database name
-define('DB_USER', 'u903322138_root'); //database user
-define('DB_PASSWORD', 'b567c63b567c63'); //database password
-define('DB_HOST', 'localhost'); //database host / use IP address to avoid DNS lookup
+define('DB_NAME', 'hell'); //database name
+define('DB_USER', 'root'); //database user
+define('DB_PASSWORD', ''); //database password
+define('DB_HOST', '127.0.0.1'); //database host / use IP address to avoid DNS lookup
 
 define('DEFAULT_CONTROLLER','Home'); //default controller if there isn't one in the url
 define('DEFAULT_LAYOUT','default'); //if no layout is set in the controller use this s_layout
-define('PROOT','/'); //set this to '/' for a live server
+define('PROOT','/framework/'); //set this to '/' for a live server
+define('POOT','/framework/'); //set this to '/' for a live server
 define('SITE_TITLE','Rufus Framework'); //This will be used if no site title is set
 define('MENU_BRAND' ,'RUFUS'); // This is the brand text in menu
 
