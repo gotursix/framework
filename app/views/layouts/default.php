b05b51d0e97d427dbcbf6fe4d8e34b694ad733f5
Zeul <vly.2814@gmail.com>
2019-03-05 23:55:27 -0800

    Fix

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -9,13 +9,11 @@ use Core\Session;
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$this->siteTitle(); ?></title>
-
     <link href="<?=PROOT?>css/custom2.css" rel="stylesheet">
     <link href="<?=PROOT?>css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?=PROOT?>css/font-awesome.min.css" >
     <link href="<?=PROOT?>css/lightgallery.css" rel="stylesheet">
     <script src="<?=PROOT?>js/jQuery-3.3.1.js"?></script>
-    <script src="<?=PROOT?>js\jquery.min.js"?></script>
     <script src="<?=PROOT?>js/open-sans_n3_default.js"?></script>
     <script src="<?=PROOT?>js/popper.min.js"></script>
     <script src="<?=PROOT?>js/bootstrap.min.js"></script>
