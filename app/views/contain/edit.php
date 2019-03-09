ad7b0185a5044297b5514f5157c3f3238be2802f
Andrew <dorinandries766@yahoo.com>
2019-03-09 18:17:39 +0200

    fix

--- app/views/contain/edit.php
+++ app/views/contain/edit.php
@@ -12,46 +12,63 @@ use Core\FH ;
 
 
 <div class="container">
-    <div class="row center">
-<h1 class=" head-form col-md-5 mx-auto formerfix">Edit <?= $this->album->name ?></h1>
-    </div>
-                <h2 class="head-form col-md-5 mx-auto formerfix center">Already in album</h2>
-        <div id="image-grid" class="container-fluid ">
-            <div class="whitebg center">
-                  <hr>
-        <a href="<?=PROOT?>album" class="btn btn-primary ">Save </a>
-          <hr>
-  <div class="row">
-     <?php if ($this->album->format == 1): ?>
-            <?php $x=1; ?>
-               <?php foreach ($this->contain as $upload): ?>
-                  <?php $dir = Users::currentUser()->id; ?>
-                     <div class="col-lg-3 col-xs-6 custom-col"  data-src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" data-sub-html="<h4><?=$upload->name ?></h4>">
 
+			<div class="row center">
+				
+				<h1 class=" head-form col-md-5 mx-auto formerfix">Album: <font class="album-name" color="red"><i><?= $this->album->name ?></i></font></h1>
+			
+			</div>
+			<div class="row center">
+			<h2 class="head-form col-md-5 mx-auto top center">Images in album</h2>
+			</div>
+			<div id="image-grid" class="container-fluid ">
+			
+			
+				
+				<div class="whitebg center">
+				
+					<hr>
+						
+						<a href="<?=PROOT?>album" class="btn btn-primary ">Save </a>
+						<hr>
+						
+						<div class="row">
+							
+							<?php if ($this->album->format == 1): ?>
+								<?php $x=1; ?>
+									<?php foreach ($this->contain as $upload): ?>
+										<?php $dir = Users::currentUser()->id; ?>
+											
+											<div class="col-lg-3 col-xs-6 custom-col"  data-src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" data-sub-html="<h4><?=$upload->name ?></h4>">
+												
+												<img src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" alt="Thumb-<?=$x?>" class="img-responsive" />
+												<br>
+												<a href="<?=PROOT?>contain/delete/<?=$upload->id?>/<?= $this->album->id ?>" class="btn btn-danger btn-xs">Remove from album</a>
 
-                              <img src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" alt="Thumb-<?=$x?>" class="img-responsive" />
-                               <br>
-                              <a href="<?=PROOT?>contain/delete/<?=$upload->id?>/<?= $this->album->id ?>" class="btn btn-danger btn-xs">Remove from album</a>
+											</div>
+											
+											<?php $x++; ?>
+									<?php endforeach; ?>
+							<?php endif; ?>
 
-                    </div>
-                 <?php $x++; ?>
-              <?php endforeach; ?>
-      <?php endif; ?>
-
-       <?php if ($this->album->format == 2): ?>
-              <?php $x=1; ?>
-                 <?php foreach ($this->contain as $upload): ?>
-                    <?php $dir = Users::currentUser()->id; ?>
-                       <div class="col-lg-3 col-xs-6 custom-col"  data-src="<?= PROOT . 'img' . DS . 'video.png' ;?>" data-sub-html="<h4><?=$upload->name ?></h4>">
-
-                    <img src="<?= PROOT . 'img' . DS . 'video.png' ;?>" alt="Thumb-<?=$x?>" class="img-responsive" />
-    <br>
-                               <a href="<?=PROOT?>contain/delete/<?=$upload->id?>/<?= $this->album->id ?>" class="btn btn-danger btn-xs">Remove from album</a>
-
-                      </div>
-                   <?php $x++; ?>
-                <?php endforeach; ?>
-         <?php endif; ?>
+							
+							
+							
+							<?php if ($this->album->format == 2): ?>
+								<?php $x=1; ?>
+									<?php foreach ($this->contain as $upload): ?>
+										<?php $dir = Users::currentUser()->id; ?>
+											
+											<div class="col-lg-3 col-xs-6 custom-col"  data-src="<?= PROOT . 'img' . DS . 'video.png' ;?>" data-sub-html="<h4><?=$upload->name ?></h4>">
+												
+												<img src="<?= PROOT . 'img' . DS . 'video.png' ;?>" alt="Thumb-<?=$x?>" class="img-responsive" />
+													<br>
+												<a href="<?=PROOT?>contain/delete/<?=$upload->id?>/<?= $this->album->id ?>" class="btn btn-danger btn-xs">Remove from album</a>
+											
+											</div>
+											<?php $x++; ?>
+									<?php endforeach; ?>
+							<?php endif; ?>
 
 
          <?php if ($this->album->format == 3): ?>
@@ -89,7 +106,7 @@ use Core\FH ;
               </div>
 
 
-              <h2 class="head-form col-md-5 mx-auto formerfix center">Add to album</h2>
+              <h2 class="head-form col-md-5 mx-auto top center">Select your images</h2>
   <div class="whitebg center">
 
 <div class="row" >
