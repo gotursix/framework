ed49cd1c02eddd32136d349d00a00badfe66a349
Zeul <vly.2814@gmail.com>
2019-03-09 16:14:51 +0200

    Email unique + token

--- app/models/Users.php
+++ app/models/Users.php
@@ -16,7 +16,7 @@ class Users extends Model
 {
    private $_isLoggedIn, $_sessionName, $_cookieName, $post_vars, $_confirm;
    public static $currentLoggedInUser = null;
-   public $id, $username , $email , $password , $fname , $lname , $acl , $deleted=0;
+   public $id, $username , $email , $password , $fname , $lname , $acl , $token ,  $deleted=0;
 
    public function __construct($user='')
    {
@@ -56,6 +56,7 @@ class Users extends Model
      $this->runValidation(new MaxValidator($this,['field'=>'username','rule'=>150,'msg'=>'Username must be less than 150 characters.']));
      $this->runValidation(new MaxValidator($this,['field'=>'email','rule'=>150,'msg'=>'Email must be less than 150 characters.']));
      $this->runValidation(new UniqueValidator($this, ['field'=>'username', 'msg'=>'This username already exists, please choose another one.']));
+     $this->runValidation(new UniqueValidator($this, ['field'=>'email', 'msg'=>'This email already belongs to an account, please choose another one.']));
      $this->runValidation(new RequiredValidator($this , ['field'=>'password' , 'msg'=>'Password is required']));
      $this->runValidation(new MinValidator($this,['field'=>'password','rule'=>6,'msg'=>'Password must be at least 6 characters.']));
      if($this->isNew())
