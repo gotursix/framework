537e91695a99d832d9aaa827cd4f2f0a5289a7b3
Zeul <vly.2814@gmail.com>
2019-03-13 17:24:00 +0200

    Delete updated design

--- app/views/upload/documents.php
+++ app/views/upload/documents.php
@@ -5,43 +5,44 @@ use Core\FH;
 ?>
 <?php $this->start('body'); ?>
 <div class="background">
-  <div class="container"><div class="content">
-    <div class="row">
-      <div id="image-grid" class="container-fluid ">
-        <div class="whitebg center formerfix">
-          <h1 class="center lg-bg col-md-5 mx-auto noselect">My documents</h1>
-          <hr>
-          <a href="<?=PROOT?>upload/add" class="btn btn-info" >
-            Upload files
-          </a>
-          <a href="<?=PROOT?>upload/modify" class="btn btn-danger" >
-            Delete files
-          </a>
-          <a href="<?=PROOT?>album/create/4" class="btn btn-info" >
-            Create album
-          </a>
-          <hr>
-          <div class="row">
-            <?php $x=1; ?>
-            <?php foreach ($this->upload as $upload): ?>
-            <?php $dir = Users::currentUser()->id; ?>
-            <div class="col-lg-3 col-xs-6 custom-col"  data-src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" data-sub-html="<h4><?=$upload->name ?></h4>">
-              <a href="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" target="_blank">
-                <img src="<?= PROOT . 'img' . DS . 'document.png' ;?>" alt="Thumb-<?=$x?>" class="img-responsive" />
-                <?= FH::hoverTag($upload->name , substr($upload->name, 0 , 25));?>
-              </a>
+  <div class="container">
+    <div class="content">
+      <div class="row">
+        <div id="image-grid" class="container-fluid ">
+          <div class="whitebg center formerfix">
+            <h1 class="center lg-bg col-md-5 mx-auto noselect">My documents</h1>
+            <hr>
+            <a href="<?=PROOT?>upload/add" class="btn btn-info" >
+              Upload files
+            </a>
+            <a href="<?=PROOT?>upload/modify" class="btn btn-danger" >
+              Delete files
+            </a>
+            <a href="<?=PROOT?>album/create/4" class="btn btn-info" >
+              Create album
+            </a>
+            <hr>
+            <div class="row">
+              <?php $x=1; ?>
+              <?php foreach ($this->upload as $upload): ?>
+              <?php $dir = Users::currentUser()->id; ?>
+              <div class="col-lg-3 col-xs-6 custom-col"  data-src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" data-sub-html="<h4><?=$upload->name ?></h4>">
+                <a href="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" target="_blank">
+                  <img src="<?= PROOT . 'img' . DS . 'document.png' ;?>" alt="Thumb-<?=$x?>" class="img-responsive" />
+                  <?= FH::hoverTag($upload->name , substr($upload->name, 0 , 25));?>
+                </a>
+              </div>
+              <?php $x++; ?>
+              <?php endforeach; ?>
             </div>
-            <?php $x++; ?>
-            <?php endforeach; ?>
+            <?php if(!$this->upload): ?>
+            <h1 class="center">There are no documents added.</h1>
+            <?php endif;?>
           </div>
-          <?php if(!$this->upload): ?>
-          <h1 class="center">There are no documents added.</h1>
-          <?php endif;?>
         </div>
       </div>
+      <script> lightGallery(document.getElementById('lightgallery')); </script>
     </div>
-    <script> lightGallery(document.getElementById('lightgallery')); </script>
   </div>
-</div>
 </div>
 <?php $this->end(); ?>
--- app/views/upload/index.php
+++ app/views/upload/index.php
@@ -24,8 +24,15 @@ use Core\FH;
 							<?php $dir = Users::currentUser()->id; ?>
 							<div class="col-lg-3 col-xs-6 custom-col" data-src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" data-sub-html="<h4><?=$upload->name ?></h4>">
 								<img src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" alt="Thumb-<?=$x?>" class="img-responsive" />
+								
 								<div class="caption center">
-									<p><?php echo substr($upload->name, 0 , 25); ?></p>
+									<div class="caption center word-break margin-file" data-toggle="collapse" href="#collapseExample<?=$x ?>" role="button" aria-expanded="false" aria-controls="collapseExample">
+										<?php echo substr($upload->name, 0 , 25); ?>
+										<div class="collapse  word-break margin-file" id="collapseExample<?=$x ?>">
+											<?php echo substr($upload->name, -strlen($upload->name)+25 , strlen($upload->name));  ?>
+										</div>
+									</div>
+									<br>
 									<a href="<?=PROOT?>upload/delete/<?=$upload->id?>" class="btn btn-danger btn-xs" onclick="if(!confirm('Are you sure ?')){return false;}">
 										Delete
 									</a>
@@ -73,8 +80,8 @@ use Core\FH;
 								</div>
 								<br>
 								<a href="<?=PROOT?>upload/delete/<?=$upload->id?>" class="btn btn-danger btn-xs" onclick="if(!confirm('Are you sure ?')){return false;}">
-										Delete
-									</a>
+									Delete
+								</a>
 							</div>
 							<?php endif;?>
 							<?php $x++; ?>
@@ -88,7 +95,6 @@ use Core\FH;
 			</div>
 		</div>
 	</div>
-
 	<div class="container">
 		<div class="content">
 			<div class="row">
@@ -118,9 +124,9 @@ use Core\FH;
 								</div>
 								<br>
 								<a href="<?=PROOT?>upload/delete/<?=$upload->id?>" class="btn btn-danger btn-xs" onclick="if(!confirm('Are you sure ?')){return false;}">
-										Delete
-									</a>
-									<br>
+									Delete
+								</a>
+								<br>
 							</div>
 							<?php $x++; ?>
 							<?php endif;?>
@@ -134,44 +140,40 @@ use Core\FH;
 			</div>
 		</div>
 	</div>
-
-
-
-	
-</div>
-<?php foreach ($this->upload as $upload): ?>
-<?= $upload->name;?>
-<?php $dir = Users::currentUser()->id; ?>
-<?php if($upload->format == 1): ?>
-<img src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" class="img-responsive">
-<?php endif; ?>
-<?php if($upload->format == 2): ?>
-<video class="img-responsive" controls>
-	<source src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" type="video/mp4">
-	Your browser does not support the video tag.
-</video>
-<?php endif; ?>
-<?php if($upload->format == 3): ?>
-<audio controls>
-	<source src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" type="audio/mpeg">
-	Your browser does not support the audio element.
-</audio>
-<?php endif; ?>
-<?php if($upload->format == 4): ?>
-<a href="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" target="_blank">View the pdf</a>
-<?php endif; ?>
-<a href="<?=PROOT?>upload/delete/<?=$upload->id?>" class="btn btn-danger btn-xs" onclick="if(!confirm('Are you sure ?')){return false;}">
-	Delete
-</a>
-<?php endforeach; ?>
-<?php if(!$this->upload): ?>
-<h1>
-You don't have any file.
-</h1>
-<?php endif; ?>
-</div>
-<br>
-</div>
-</div>
+	<div class="container">
+		<div class="content">
+			<div class="row">
+				<div id="image-grid" class="container-fluid ">
+					<div class="whitebg center formerfix">
+						<h1 class="center lg-bg col-md-5 mx-auto noselect">My documents</h1>
+						<hr>
+						<a href="<?=PROOT?>upload/modify" class="btn btn-danger" >
+							Delete all documents
+						</a>
+						<hr>
+						<div class="row">
+							<?php $x=1; ?>
+							<?php foreach ($this->upload as $upload): ?>
+							<?php if($upload->format == 4): ?>
+							<?php $dir = Users::currentUser()->id; ?>
+							<div class="col-lg-3 col-xs-6 custom-col"  data-src="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" data-sub-html="<h4><?=$upload->name ?></h4>">
+								<a href="<?= PROOT . 'files' . DS . $dir  . DS . $upload->name ;?>" target="_blank">
+									<img src="<?= PROOT . 'img' . DS . 'document.png' ;?>" alt="Thumb-<?=$x?>" class="img-responsive" />
+									<?= FH::hoverTag($upload->name , substr($upload->name, 0 , 25));?>
+								</a>
+							</div>
+							<?php endif;?>
+							<?php $x++; ?>
+							<?php endforeach; ?>
+						</div>
+						<?php if(!$this->upload): ?>
+						<h1 class="center">There are no documents added.</h1>
+						<?php endif;?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<br>
 </div>
 <?php $this->end(); ?>
