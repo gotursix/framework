f43e420f8efff2f67a3f867f4384a3fe8cf8e961
Zeul <vly.2814@gmail.com>
2019-03-15 22:47:19 +0200

    restricted design

--- app/views/restricted/badToken.php
+++ app/views/restricted/badToken.php
@@ -1,4 +1,18 @@
 <?php $this->setSiteTitle('Bad Token'); ?>
 <?php $this->start('body'); ?>
-<h1 class="text-center red m-top">Your token is corrupted.</h1>
+<div class="background ">
+    <div class="container">
+                <div class="content">
+                  <div class="row">
+                    <div id="image-grid" class="container-fluid ">
+                      <div class="whitebg center formerfix">
+                        <h1 class="center lg-bg col-md-5 mx-auto ">Corrupted token</h1>
+                        <hr><br>
+                        <h1 class="center">This could be caused by an csrf attack or by reloading a page with errors. If this is the case , please go back to the page.</h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </div>
 <?php $this->end(); ?>
--- app/views/restricted/index.php
+++ app/views/restricted/index.php
@@ -1,4 +1,18 @@
 <?php $this->setSiteTitle('Access Restricted'); ?>
 <?php $this->start('body'); ?>
-<h1 class="text-center red m-top">You do not have permission to access this page.</h1>
+<div class="background ">
+    <div class="container">
+                <div class="content">
+                  <div class="row">
+                    <div id="image-grid" class="container-fluid ">
+                      <div class="whitebg center formerfix">
+                        <h1 class="center lg-bg col-md-5 mx-auto ">Access Restricted</h1>
+                        <hr><br>
+                        <h1 class="center">You have no permission to access this page.</h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </div>
 <?php $this->end(); ?>
