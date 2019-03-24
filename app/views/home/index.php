04f776b851cacec898814a68b1ff1c8ac64a3d16
Popescu Valentin-Dimitrie <popescuvalentin16768@gmail.com>
2019-03-24 18:11:05 +0200

    carousel
    fara imagini

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -6,37 +6,76 @@ use App\Models\Users;
 <?php $this->setSiteTitle('Home'); ?>
 <?php $this->start('body'); ?>
 <div class="background ">
-  <div class="container">
-    <br>
-    <br>
-    <br>
-    <div class="whitebg m-top">
-      <h1 class="center ">Welcome to Rufus Framework</h1>
-      <div class="center" onclick="ajaxTest();">Button me :>></div>
-     
-      <?php if(Users::currentUser() &&  preg_match('/(Chrome|CriOS)\//i',$_SERVER['HTTP_USER_AGENT']) && !preg_match('/(Aviator|ChromePlus|coc_|Dragon|Edge|Flock|Iron|Kinza|Maxthon|MxNitro|Nichrome|OPR|Perk|Rockmelt|Seznam|Sleipnir|Spark|UBrowser|Vivaldi|WebExplorer|YaBrowser)/i',$_SERVER['HTTP_USER_AGENT']) )	: ?>
-       <h1 class="center ">Aici comenzi vocale.</h1>
-  	 <?php endif; ?>
+    <div class="content">
+        <div class="row">
+            <div id="image-grid" class="container-fluid ">
+                <div class="whitebg center formerfix">
+                    <h1 class="center ">Welcome to Rufus Framework</h1>
+                    <div class="center" onclick="ajaxTest();">Button me :>></div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 hidden-xs">
 
+                        <div id="carousel-299058" class="carousel slide">
 
+                            <ol class="carousel-indicators">
+
+                                <li data-target="#carousel-299058" data-slide-to="0" class=""> </li>
+
+                                <li data-target="#carousel-299058" data-slide-to="1" class="active"> </li>
+
+                                <li data-target="#carousel-299058" data-slide-to="2" class=""> </li>
+
+                            </ol>
+
+                            <div class="carousel-inner">
+
+                                <div class="item"> <img class="img-responsive" src="<?=PROOT?>img/" alt="thumb">
+
+                                    <div class="carousel-caption"> </div>
+
+                                </div>
+
+                                <div class="item active"> <img class="img-responsive" src="<?=PROOT?>img/" alt="thumb">
+
+                                    <div class="carousel-caption"> </div>
+
+                                </div>
+
+                                <div class="item"> <img class="img-responsive" src="<?=PROOT?>img/" alt="thumb">
+
+                                    <div class="carousel-caption"></div>
+
+                                </div>
+
+                            </div>
+
+                            <a class="left carousel-control" href="#carousel-299058" data-slide="prev"><span class="icon-prev"></span></a> <a class="right carousel-control" href="#carousel-299058" data-slide="next"><span class="icon-next"></span></a>
+                        </div>
+
+                    </div>
+                    
+                    <h1 class="center ">Aici comenzi vocale.</h1>
+
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 </div>
 <script>
-function ajaxTest(){
-$.ajax({
-type: "POST",
-url : '<?=PROOT?>home/testAjax',
-data : {model_id:45},
-success : function(resp){
-if(resp.success){
-window.alert(resp.data.name);
-}
-console.log(resp);
-}
-});
-}
+    function ajaxTest() {
+        $.ajax({
+            type: "POST",
+            url: '<?=PROOT?>home/testAjax',
+            data: {
+                model_id: 45
+            },
+            success: function(resp) {
+                if (resp.success) {
+                    window.alert(resp.data.name);
+                }
+                console.log(resp);
+            }
+        });
+    }
+
 </script>
-</div>
 <?php $this->end(); ?>
