f8ed670589886063ec07c8daebb3738c24787432
Popescu Valentin-Dimitrie <popescuvalentin16768@gmail.com>
2019-03-24 20:30:19 +0200

    ceva mic
    stash ul vietii

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -12,49 +12,76 @@ use App\Models\Users;
                 <div class="whitebg center formerfix">
                     <h1 class="center ">Welcome to Rufus Framework</h1>
                     <div class="center" onclick="ajaxTest();">Button me :>></div>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 hidden-xs">
 
-                        <div id="carousel-299058" class="carousel slide">
+                    <div id="demo" class="carousel slide" data-ride="carousel">
 
-                            <ol class="carousel-indicators">
+                        <ul class="carousel-indicators">
+                            <li data-target="#demo" data-slide-to="0" class="active"></li>
+                            <li data-target="#demo" data-slide-to="1"></li>
+                            <li data-target="#demo" data-slide-to="2"></li>
+                            <li data-target="#demo" data-slide-to="3"></li>
+                            <li data-target="#demo" data-slide-to="4"></li>
+                            <li data-target="#demo" data-slide-to="5"></li>
+                        </ul>
 
-                                <li data-target="#carousel-299058" data-slide-to="0" class=""> </li>
-
-                                <li data-target="#carousel-299058" data-slide-to="1" class="active"> </li>
-
-                                <li data-target="#carousel-299058" data-slide-to="2" class=""> </li>
-
-                            </ol>
-
-                            <div class="carousel-inner">
-
-                                <div class="item"> <img class="img-responsive" src="<?=PROOT?>img/" alt="thumb">
-
-                                    <div class="carousel-caption"> </div>
-
-                                </div>
-
-                                <div class="item active"> <img class="img-responsive" src="<?=PROOT?>img/" alt="thumb">
-
-                                    <div class="carousel-caption"> </div>
-
-                                </div>
-
-                                <div class="item"> <img class="img-responsive" src="<?=PROOT?>img/" alt="thumb">
-
-                                    <div class="carousel-caption"></div>
-
-                                </div>
-
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="img-responsive" src="<?=PROOT?>img/1.jpg" alt="thumb">
                             </div>
-
-                            <a class="left carousel-control" href="#carousel-299058" data-slide="prev"><span class="icon-prev"></span></a> <a class="right carousel-control" href="#carousel-299058" data-slide="next"><span class="icon-next"></span></a>
+                            <div class="carousel-item">
+                                <img class="img-responsive" src="<?=PROOT?>img/2.jpg" alt="thumb">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="img-responsive" src="<?=PROOT?>img/3.jpg" alt="thumb">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="img-responsive" src="<?=PROOT?>img/4.jpg" alt="thumb">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="img-responsive" src="<?=PROOT?>img/5.jpg" alt="thumb">
+                            </div>
                         </div>
 
-                    </div>
-                    
-                    <h1 class="center ">Aici comenzi vocale.</h1>
+                        <a class="carousel-control-prev" href="#demo" data-slide="prev" id="back">
+                            <span class="carousel-control-prev-icon"></span>
+                        </a>
+                        <a class="carousel-control-next" href="#demo" data-slide="next" id="next">
+                            <span class="carousel-control-next-icon"></span>
+                        </a>
 
+                    </div>
+
+                    <h1 class="center ">Aici comenzi vocale.</h1>
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item">
+                                <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: First slide">
+                                    <title>Placeholder</title>
+                                    <rect width="100%" height="100%" fill="#777"></rect><text x="50%" y="50%" fill="#555" dy=".3em">First slide</text>
+                                </svg>
+                            </div>
+                            <div class="carousel-item active">
+                                <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Second slide">
+                                    <title>Placeholder</title>
+                                    <rect width="100%" height="100%" fill="#666"></rect><text x="50%" y="50%" fill="#444" dy=".3em">Second slide</text>
+                                </svg>
+                            </div>
+                            <div class="carousel-item">
+                                <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Third slide">
+                                    <title>Placeholder</title>
+                                    <rect width="100%" height="100%" fill="#555"></rect><text x="50%" y="50%" fill="#333" dy=".3em">Third slide</text>
+                                </svg>
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true" id="back"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true" id="next"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
